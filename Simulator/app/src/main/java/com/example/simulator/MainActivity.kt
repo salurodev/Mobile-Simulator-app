f60076bd82ddf2bab64ee4ad4db3dd7d71a2ee8a
Sandro Luiz <salurodev@gmail.com>
2022-06-13 21:16:12 -0300

    iniciando o relais1e

--- Simulator/app/src/main/java/com/example/simulator/MainActivity.kt
+++ Simulator/app/src/main/java/com/example/simulator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        //val tvHello = findViewById<TextView>(R.id.tvHello)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvHello.text = "Teste"
     }
 }
